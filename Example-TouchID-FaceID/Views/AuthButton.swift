023f7f71d1a37a097d39b6653ecaccd2e1979c6d
Justyna <kowalkowska.just@gmail.com>
2021-03-10 18:58:49 +0100

    Creating a AuthButton class to create Login/SignUp button.'

---
+++ Example-TouchID-FaceID/Views/AuthButton.swift
@@ -0,0 +1,26 @@
+//
+//  AuthButton.swift
+//  Example-TouchID-FaceID
+//
+//  Created by Justyna Kowalkowska on 10/03/2021.
+//
+
+import UIKit
+
+class AuthButton: UIButton {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        setTitleColor(UIColor.white, for: .normal)
+        backgroundColor = .circleViewBackgroundColor
+        layer.cornerRadius = 3
+        heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
